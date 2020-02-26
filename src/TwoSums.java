91925df4af0c4397240f5963c3354ddcfeb7042f
IreneSSSL <59592016+IreneSSSL@users.noreply.github.com>
2020-02-25 20:20:56 -0500

    add gitignore file and solving method for TwoSums

--- src/TwoSums.java
+++ src/TwoSums.java
@@ -10,6 +10,11 @@ public class TwoSums {
             return null;
         }
         HashMap<Integer, Integer> hashmap = new HashMap<Integer, Integer>();
+//      since this problem aims to find two numbers, it means if one is fixed, the other is fixed too.
+//      assuming we fix the first one value, then we only need to find whether the other number exists or not.
+//      if it does exist, then we could return their indice, which are stored at the value part of the ArrayList;
+//      else the assumed one is not what we want.
+//      jump to next number
         for (int i=0; i< nums.length;i++){
             Integer current = nums[i];
             Integer sub = target-current;
@@ -21,3 +26,6 @@ public class TwoSums {
         return null;
     }
 }
+
+// if code without IDE, points needed to be remembered:
+// 1 for array of int, use xx.length to get the length
